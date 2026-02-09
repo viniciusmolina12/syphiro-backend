233fa081c8871474891e62400bc72592909c0252
Vinicius Molina <viniciusgranmolina@gmail.com>
2026-02-09 08:38:56 -0300

    test: add character in memory database tests

---
+++ src/character/infrastructure/database/in-memory/character-in-memory.database.spec.ts
@@ -0,0 +1,14 @@
+import { InstanceId } from "../../../../instance/domain/instance.aggregate";
+import { PlayerId } from "../../../../player/domain/player.aggregate";
+import { Character } from "../../../domain/character.aggregate";
+import { Class, ClassId } from "../../../domain/entities/class";
+import { CharacterInMemoryDatabase } from "./character-in-memory.database";
+
+describe('CharacterInMemoryDatabase', () => {
+    it('should be able to create a character', async () => {
+        const characterInMemoryDatabase = new CharacterInMemoryDatabase();
+        const character = Character.create({instance_id: new InstanceId('1'), class: new Class({id: new ClassId('1'), name: 'Mago', description: 'Mago', icon: 'mago.png', skills: []}), player_id: new PlayerId('1')}).ok;
+        await characterInMemoryDatabase.create(character);
+        expect(characterInMemoryDatabase['characters'].length).toBe(1);
+    });
+});
