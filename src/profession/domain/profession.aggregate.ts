304fdb23398794e7dac31259dae20c2a41f2f89d
Vinicius Molina <viniciusgranmolina@gmail.com>
2026-04-16 08:08:51 -0300

    feat(profession): add name property and static create factory method

--- src/profession/domain/profession.aggregate.ts
+++ src/profession/domain/profession.aggregate.ts
@@ -1,11 +1,26 @@
 import { EntityId } from "../../@shared/entity-id.vo";
+import { Either } from "../../@shared/either";
 
 export class ProfessionId extends EntityId {}
 
+interface ProfessionConstructorProps {
+    id: ProfessionId;
+    name: string;
+}
+interface ProfessionCreateCommand {
+    id: ProfessionId;
+    name: string;
+}
 export class Profession {
-    private constructor(
-        public readonly id: ProfessionId,
-    ) {
-        this.id = id;
+    public readonly id: ProfessionId;
+    public readonly name: string;
+    private constructor(props: ProfessionConstructorProps) {    
+        this.id = props.id ?? new ProfessionId();
+        this.name = props.name;
+    }
+    
+
+    static create(command: ProfessionCreateCommand): Either<Profession, Error> {
+        return Either.safe(() => new Profession({ id: command.id, name: command.name }));
     }
 }
