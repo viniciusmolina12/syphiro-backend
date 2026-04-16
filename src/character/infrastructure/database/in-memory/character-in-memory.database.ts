d1c9aff1c34b9695145600055d25fc31d59ca15a
Vinicius Molina <viniciusgranmolina@gmail.com>
2026-04-16 08:21:28 -0300

    fix(character): implement update method in CharacterInMemoryDatabase

--- src/character/infrastructure/database/in-memory/character-in-memory.database.ts
+++ src/character/infrastructure/database/in-memory/character-in-memory.database.ts
@@ -9,6 +9,13 @@ export class CharacterInMemoryDatabase implements ICharacterRepository {
         return this.characters.find(c => c.id.equals(id)) ?? null;
     }
 
+    async update(character: Character): Promise<void> {
+        const index = this.characters.findIndex(c => c.id.equals(character.id));
+        if (index !== -1) {
+            this.characters[index] = character;
+        }
+    }
+
     async existsByIds(ids: CharacterId[], instance_id: InstanceId): Promise<boolean> {
         return this.characters.some(c => ids.some(id => c.id.equals(id)) && c.instance_id.equals(instance_id));
     }
